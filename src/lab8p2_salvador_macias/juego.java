72fdaa867dc12c97612e4cbdbdeb92f72959ee6f
papilon10 <salvigna17@gmail.com>
2025-06-13 13:19:40 -0600

    creacion clase juego

---
+++ src/lab8p2_salvador_macias/juego.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab8p2_salvador_macias;
+
+/**
+ *
+ * @author Apple
+ */
+public class juego {
+    
+}

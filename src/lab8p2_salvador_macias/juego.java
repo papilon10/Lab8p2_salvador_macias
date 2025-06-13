a42119e2f714f5825b79931cba7ec88fc19a4de5
papilon10 <salvigna17@gmail.com>
2025-06-13 13:22:43 -0600

    clase juego hecha

--- src/lab8p2_salvador_macias/juego.java
+++ src/lab8p2_salvador_macias/juego.java
@@ -9,5 +9,57 @@ package lab8p2_salvador_macias;
  * @author Apple
  */
 public class juego {
-    
+
+    private String Nombre;
+    private String Precio;
+    private String Developer;
+    private String Genero;
+
+    public juego(String Nombre, String Precio, String Developer, String Genero) {
+        this.Nombre = Nombre;
+        this.Precio = Precio;
+        this.Developer = Developer;
+        this.Genero = Genero;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getPrecio() {
+        return Precio;
+    }
+
+    public void setPrecio(String Precio) {
+        this.Precio = Precio;
+    }
+
+    public String getDeveloper() {
+        return Developer;
+    }
+
+    public void setDeveloper(String Developer) {
+        this.Developer = Developer;
+    }
+
+    public String getGenero() {
+        return Genero;
+    }
+
+    public void setGenero(String Genero) {
+        this.Genero = Genero;
+    }
+
+    @Override
+    public String toString() {
+        return "nombre del juego: " + Nombre
+                + "\nprecio del juego: " + Precio
+                + "\ndesarrollador del juego: " + Developer
+                + "\ngenero del juego: " + Genero;
+    }
+
 }
